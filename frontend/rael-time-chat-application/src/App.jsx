092645c051e9ffa5caa913056b64086915cbb26f
Ritika chaurasiya <shaksichaurasiyasc0207@gmail.com>
2026-07-02 15:58:06 +0530

    debug: log clerk token

--- frontend/rael-time-chat-application/src/App.jsx
+++ frontend/rael-time-chat-application/src/App.jsx
@@ -7,11 +7,12 @@ import { useAuth } from "@clerk/react";
 import PageLoader from "./components/PageLoader";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
+import { axiosInstance } from "./lib/axios";
 
 import { Toaster } from "react-hot-toast";
 
 function App() {
-  const { isSignedIn, isLoaded } = useAuth();
+  const { isSignedIn, isLoaded, getToken } = useAuth();
 
   // option 1
   // const { checkAuth, isCheckingAuth, clearAuth } = useAuthStore();
@@ -21,12 +22,33 @@ function App() {
   const checkAuth = useAuthStore((state) => state.checkAuth);
   const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
 
+  // attach Clerk token to every axios request
+  useEffect(() => {
+    const id = axiosInstance.interceptors.request.use(async (config) => {
+      const token = await getToken();
+      if (token) config.headers.Authorization = `Bearer ${token}`;
+      return config;
+    });
+
+    return () => axiosInstance.interceptors.request.eject(id);
+  }, [getToken]);
+
   useEffect(() => {
     if (!isLoaded) return;
 
     if (isSignedIn) checkAuth();
     else clearAuth();
   }, [checkAuth, clearAuth, isLoaded, isSignedIn]);
+  useEffect(() => {
+  const id = axiosInstance.interceptors.request.use(async (config) => {
+    const token = await getToken();
+    console.log("CLERK TOKEN:", token);
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  });
+
+  return () => axiosInstance.interceptors.request.eject(id);
+}, [getToken]);
 
   if (!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
 
